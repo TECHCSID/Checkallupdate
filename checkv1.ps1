adb044564eccbe64112cd65e6b645259f777fa32
TECHCSID <30695095+TECHCSID@users.noreply.github.com>
2022-06-08 11:00:26 +0200

    Update checkv1.ps1

--- checkv1.ps1
+++ checkv1.ps1
@@ -2,4 +2,7 @@ $inotversion = (Get-Item E:\FrameWork.CSID\San\i-Not\Builds\GenApi.iNot.Client.e
 $booksversion = (Get-Itemproperty -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\GenApi\Finance\Serveur).version
 $pendingupdate = (Get-ChildItem -path C:\"Program Files (x86)"\CSID\"CSiD Update"\Archives\ -Recurse -Force -name)
 $synchroversion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo("C:\Program Files (x86)\CSiD\Synchro iNot Exchange\SynchroInot.exe").FileVersion
-Write-Output " Inot=$inotversion Books=$booksversion Synchro=$synchroversion Pending=$pendingupdate " 
+$ApplicationTmp = Select-String -Path 'C:\Program Files (x86)\CSiD\CSiD Update\paramgu.ini' -Pattern Application 
+$Application = ($ApplicationTmp -split '=')[1]
+
+Write-Output " Inot:$inotversion Books:$booksversion Synchro:$synchroversion List:$Application Pending:$pendingupdate  " 

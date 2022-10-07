0605d7d58449c64282d82c480a9bd28cd47094f6
TECHCSID <30695095+TECHCSID@users.noreply.github.com>
2022-10-07 14:19:21 +0200

    Update and rename t to PreCheckInstallV5.ps1

--- PreCheckInstallV5.ps1
+++ PreCheckInstallV5.ps1
@@ -1,4 +1,4 @@
-$errors = "Pas d'erreurs"
+$errors = "Pas d'erreur"
 
 #Récupréation de la valeur
 $NumEtudeTmp = Select-String -Path 'C:\Program Files (x86)\CSiD\CSiD Update\paramgu.ini' -Pattern Numero 
@@ -19,21 +19,39 @@ $inotPath = Get-ItemProperty -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\GenApi\
     }
 }
 if  ($null -eq $inotPath) {
-        $drives = Get-PSDrive -PSProvider FileSystem | Foreach-Object { if(Test-Path -Path $_.Root){get-childitem $_.Root | Where-Object {$_.PSIsContainer -eq $true -and $_.Name -match "FrameWork.CSID"} } }
+        $drives = Get-PSDrive -PSProvider FileSystem | Foreach-Object { if(Test-Path -Path $_.Root){get-childitem $_.Root  | Where-Object {$_.PSIsContainer -eq $true -and $_.Name -match "FrameWork.CSID"} } }
 
         if($null -ne $drives)
         {
-            $inotPath = Join-Path -Path $drives[0].Root -ChildPath "Framework.CSID"; 
+            $inotPath = $drives[0].FullName; 
             if($drives.Count -gt 1)
             {
-                $errors = "Plusieurs disques trouves contenant Framework.CSID, attention aux problemes d installations";
+                $errors = "Several disks with Framework.CSID, WARNING for install";
             }
 
             if(-Not (Test-Path -Path "$inotPath\San\i-Not\Builds\"))
             {
                 if($drives.Count -gt 1)
                 {
-                    $inotPath = Join-Path -Path $drives[1].Root -ChildPath "Framework.CSID";
+                    $inotPath = $drives[1].FullName;
+                }
+            }
+        } else {
+            $drives = Get-PSDrive -PSProvider FileSystem | Foreach-Object { if(Test-Path -Path $_.Root) { get-childitem $_.Root -recurse -ErrorAction SilentlyContinue  | Where-Object {$_.PSIsContainer -eq $true -and $_.Name -match "FrameWork.CSID"} } }
+            if($null -ne $drives)
+            {
+                $inotPath = $drives[0].FullName; 
+                if($drives.Count -gt 1)
+                {
+                    $errors = "Several disks with Framework.CSID, WARNING for install";
+                }
+    
+                if(-Not (Test-Path -Path "$inotPath\San\i-Not\Builds\"))
+                {
+                    if($drives.Count -gt 1)
+                    {
+                        $inotPath = $drives[1].FullName;
+                    }
                 }
             }
         }
@@ -50,21 +68,31 @@ $synchroPath = $null;
 $synchroService = Get-WmiObject win32_service | ?{$_.Name -like 'Synchronisation iNot Exchange'};
 if($null -ne $synchroService)
 {
-    $synchroPath = $synchroService.PathName.Trim().Trim('"')
+    $synchroPath = $synchroService.PathName.Trim().Trim('"');
+} else {
+    $synchroService = Get-WmiObject win32_service | ?{$_.Name -like 'Synchro iNot Exchange'};
+    if($null -ne $synchroService)
+    {
+        $synchroPath = $synchroService.PathName.Trim().Trim('"');
+    }
 } 
 
 #récupération des version de Inot, Books et synchro
-$inotversion = (Get-Item $inotPath\San\i-Not\Builds\GenApi.iNot.Client.exe).VersionInfo.ProductVersion;
+if($null -ne $inotPath) {
+    $inotversion = (Get-Item $inotPath\San\i-Not\Builds\GenApi.iNot.Client.exe).VersionInfo.ProductVersion;
+} else {
+    $inotversion = "No iNot Office";
+}
 if(Test-Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\GenApi\Finance\Serveur)
 {
     $booksversion = (Get-Itemproperty -Path Registry::HKEY_LOCAL_MACHINE\SOFTWARE\GenApi\Finance\Serveur).version;
 } else {
-    $booksversion = "Pas de books";
+    $booksversion = "No Books";
 }
 if($null -ne $synchroPath) {
 $synchroversion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo($synchroPath).FileVersion;
 } else {
-    $synchroversion = " Pas de Synchronisation Exchange";
+    $synchroversion = " No Synchro";
 }
 $pendingupdate = (Get-ChildItem -path $csidArchivePath -Force -name);
 $ApplicationTmp = Select-String -Path $csidUpdatePath\paramgu.ini -Pattern Application;
